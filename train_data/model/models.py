ec0c0b4432aab8e43ddf1797d340d049a7ba164b
rainavyas <sayvaniar@gmail.com>
2024-08-27 00:42:23 +0100

    prep squad data concepts data

---
+++ train_data/model/models.py
@@ -0,0 +1,59 @@
+from openai import OpenAI
+import openai
+
+with open('/home/ubuntu/keys/openai.txt', 'r') as f:
+    key_str = f.read()
+API_KEY = key_str.strip('\n')
+openai.api_key = API_KEY
+
+with open('/home/vr313/rds/hpc-work/Projects/Control/multi-concept-steering-llm/experiments/keys/openai.txt', 'r') as f:
+    key_str = f.read()
+PERPLEXITY_API_KEY = key_str.strip('\n')
+
+OPENAI_MODELS = {
+    "gpt3.5": "gpt-3.5-turbo",
+    "gpt4": "gpt-4-turbo",
+    "gpt4o": "gpt-4o",
+}
+
+class OpenAIModel:
+    """Class wrapper for models that interact with a OpenAI API"""
+
+    def __init__(self, model_name = "gpt4o", api_key='', system_prompt=None):
+        self.model_name = OPENAI_MODELS[model_name]
+        self.client = OpenAI(api_key=api_key)
+        self.system_prompt = system_prompt
+    
+    def predict(self, prompt):
+        system_msg = {"role": "system", "content": self.system_prompt}
+        user_msg = {"role": "user", "content": prompt}
+        if self.system_prompt is None:
+            response = self.client.chat.completions.create(
+                model=self.model_name, messages=[user_msg], temperature=0
+            )
+        else:
+            response = self.client.chat.completions.create(
+                model=self.model_name, messages=[system_msg, user_msg], temperature=0
+            )
+        return response.choices[0].message.content
+
+
+    def predict_with_logits(self, prompt, max_tokens=1, logit_bias={}):
+
+        completion = self.client.chat.completions.create(
+            model = self.model_name,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            max_tokens = max_tokens,
+            logit_bias = logit_bias,
+            seed = 1,
+            logprobs = True,
+            top_logprobs = 20,
+        )
+
+        sequence_token_logits = completion.choices[0].logprobs.content
+        return sequence_token_logits
--- train_data/question_generation/prep_concepts_data.py
+++ train_data/question_generation/prep_concepts_data.py
@@ -1,1 +1,51 @@
-# TODO
+from .load_raw_data import select_data
+from train_data.model.models import OpenAIModel
+
+import random
+from tqdm import tqdm
+import json
+
+random.seed(42)
+
+def prep_squad(save_path, lim=1000):
+    _, data = select_data('squad')
+
+    # randomly select <lim> data points
+    sampled_data = random.sample(data, lim)
+    inputs = get_data_inputs('squad', sampled_data)
+
+    # get concept outputs prepped data
+    instruction = 'Given the input context, generate a question on the context to <DIRECTION> the <CONCEPT> of the question generated. Return only the output question and nothing else.'
+    concepts = ['formality', 'difficulty', 'language complexity']
+    concept_descriptions = ['How formal the tone of the question is.', 'How challenging the question is based on how difficult it is to find the answer in the context.', 'The complexity of the language choice in the question.']
+    prepped_data = prep_concepts_data(inputs, instruction, concepts, concept_descriptions)
+
+    # save prepped data
+    with open(save_path, 'w') as f:
+        json.dump(prepped_data, f)
+
+def get_data_inputs(data_name, data):
+    '''
+        Get the inputs to generate outputs
+    '''
+    if data_name == 'squad':
+        inputs = [d['context'] for d in data]
+    return inputs
+
+def prep_concepts_data(inputs, instruction, concepts, concept_descriptions, directions=['maximize', 'minimize']):
+    llm = OpenAIModel('gpt-4o')
+    prepped_data = []
+    for input in tqdm(inputs):
+        output_concept = {}
+        for concept, description in zip(concepts, concept_descriptions):
+            for direction in directions:
+                curr_instruction = instruction.replace('<CONCEPT>', concept).replace('<DIRECTION>', direction)
+                prompt = f"{curr_instruction}\nDefinition of {concept}: {description}\nInput: {input}\nOutput:\n\n"
+                out = llm.predict(prompt)
+                if concept not in output_concept:
+                    output_concept[concept] = {}
+                output_concept[concept][direction] = out
+        prepped_data.append({'input': input, 'output': output_concept})
+    return prepped_data
+
+
